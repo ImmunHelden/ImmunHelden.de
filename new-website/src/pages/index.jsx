c84c14daa3db6518337bd9773cb72dbd52044ee3
David Kagerer <david.kagerer@prosiebensat1digital.de>
2020-05-05 22:18:37 +0200

    remove wip code

--- new-website/src/pages/index.jsx
+++ new-website/src/pages/index.jsx
@@ -3,18 +3,12 @@ import Layout from "../components/layout"
 import SEO from "../components/seo"
 import Banner from "../components/banner/Banner"
 import { JoinLane } from "../components/join-lane"
-import { VerticalImageLane } from "../components/layout/vertical-image-lane"
 
 const IndexPage = () => {
     return (
         <Layout>
             <SEO title="Home" />
             <Banner />
-            <JoinLane />
-            <JoinLane />
-            <VerticalImageLane />
-            <JoinLane />
-            <JoinLane />
             <JoinLane />
         </Layout>
     )
