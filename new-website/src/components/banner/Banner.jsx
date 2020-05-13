688bc6c44ea6f22e4638e189b59f9c5b9f55ced3
David Kagerer <david@binder-kagerer.de>
2020-05-13 22:52:25 +0200

    refactor: :lipstick: Change Header to new style
    Changes the header to the new style for the plasma page

--- new-website/src/components/banner/Banner.jsx
+++ new-website/src/components/banner/Banner.jsx
@@ -1,44 +1,21 @@
 import React from "react"
 import BannerImage from "./BannerImage"
-import styled from "styled-components"
-import { FormattedMessage } from "gatsby-plugin-intl"
+import { makeStyles } from "@material-ui/core"
 
-const Banner = styled.section`
-    color: white;
-    display: flex;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-`
-const TextBox = styled.div`
-    position: absolute;
-    z-index: 3;
-    text-align: center;
-    left: 0;
-    right: 0;
-`
-
-const Title = styled.h1`
-    color: white;
-`
-const Description = styled.h2`
-    font-size: 1.25rem;
-    text-shadow: 1px 1px #333;
-    color: #fff;
-`
+const useStyles = makeStyles(theme => ({
+    banner: {
+        color: "white",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+    },
+}))
 
 export default function BannerComponent() {
+    const classes = useStyles()
     return (
-        <Banner>
-            <BannerImage className="banner" />
-            <TextBox>
-                <Title>
-                    <FormattedMessage id="pageTitle" />
-                </Title>
-                <Description>
-                    <FormattedMessage id="pageDescription" />
-                </Description>
-            </TextBox>
-        </Banner>
+        <section className={classes.banner}>
+            <BannerImage />
+        </section>
     )
 }
