328649aba64141d49532a2d498899d3b6678521a
David Kagerer <david@binder-kagerer.de>
2020-05-20 13:30:30 +0200

    feat: :sparkles: add immunhero register form

--- new-website/src/components/banner/Banner.jsx
+++ new-website/src/components/banner/Banner.jsx
@@ -1,6 +1,6 @@
 import React from "react"
 import BannerImage from "./BannerImage"
-import { makeStyles } from "@material-ui/core"
+import { makeStyles, Grid } from "@material-ui/core"
 
 const useStyles = makeStyles(theme => ({
     banner: {
@@ -8,14 +8,22 @@ const useStyles = makeStyles(theme => ({
         display: "flex",
         flexDirection: "column",
         justifyContent: "center",
+        zIndex: 1,
+    },
+    content: {
+        position: "absolute",
+        zIndex: 2,
     },
 }))
 
-export default function BannerComponent() {
+export default function BannerComponent({ children }) {
     const classes = useStyles()
     return (
         <section className={classes.banner}>
             <BannerImage />
+            <Grid container xs={12} className={classes.content} justify="flex-end">
+                {children}
+            </Grid>
         </section>
     )
 }
--- new-website/src/pages/index.jsx
+++ new-website/src/pages/index.jsx
@@ -2,12 +2,23 @@ import React from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import Banner from "../components/banner/Banner"
+import { ImmuneHeroContactFrom } from "../components/immune-hero/contact"
+import { makeStyles } from "@material-ui/styles"
+
+const useStyles = makeStyles(theme => ({
+    topBannerArea: {
+        position: "absolute",
+    },
+}))
 
 const IndexPage = () => {
+    const classes = useStyles()
     return (
         <Layout>
             <SEO title="Home" />
-            <Banner />
+            <Banner>
+                <ImmuneHeroContactFrom />
+            </Banner>
         </Layout>
     )
 }
