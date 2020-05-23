090190efc7a46f377c41012ef5f192e42bd41784
David Kagerer <david@binder-kagerer.de>
2020-05-24 00:01:21 +0200

    feat(partner): :sparkles: add partner to provider

--- new-website/src/components/protected.jsx
+++ new-website/src/components/protected.jsx
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
 import { navigate } from "gatsby-plugin-intl"
 import { useAuth } from "../hooks/use-auth"
 import firebase from "gatsby-plugin-firebase"
@@ -6,6 +6,20 @@ import { userContext } from "../hooks/use-session"
 
 export const Protected = ({ children, loginUrl }) => {
     const { initializing, user } = useAuth(firebase)
+    const [partners, setPartners] = useState(null)
+
+    useEffect(() => {
+        async function getPartners() {
+            if (user) {
+                const res = await firebase.firestore().collection("users").doc("iHEgHzEO6Xbdk7cuUfMKtSdIt6q2").get()
+                if (res.exists) {
+                    setPartners(res.data().partners)
+                }
+            }
+        }
+        getPartners()
+    }, [user])
+
     if (initializing) {
         return <h1>...</h1>
     }
@@ -15,5 +29,5 @@ export const Protected = ({ children, loginUrl }) => {
         return <error>NotLoggedIn</error>
     }
 
-    return <userContext.Provider value={{ user }}>{children}</userContext.Provider>
+    return <userContext.Provider value={{ user, partners }}>{children}</userContext.Provider>
 }
