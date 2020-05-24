1dce361c58c23755e47778b4fa8937871ebcd357
David Kagerer <david@binder-kagerer.de>
2020-05-24 20:41:44 +0200

    feat(partner): :sparkles: Add verify email screen

--- new-website/gatsby-config.js
+++ new-website/gatsby-config.js
@@ -78,6 +78,14 @@ module.exports = {
                 },
             },
         },
+        {
+            resolve: "gatsby-plugin-react-svg",
+            options: {
+                rule: {
+                    include: /svg/,
+                },
+            },
+        },
         // this (optional) plugin enables Progressive Web App + Offline functionality
         // To learn more, visit: https://gatsby.dev/offline
         // `gatsby-plugin-offline`,
--- new-website/src/components/login-register-form/index.jsx
+++ new-website/src/components/login-register-form/index.jsx
@@ -3,36 +3,116 @@ import { navigate } from "gatsby-plugin-intl"
 import firebase from "gatsby-plugin-firebase"
 import Login from "./login"
 import Register from "./register"
-import { isNode } from "@firebase/util"
 import { useAuth } from "../../hooks/use-auth"
+import MuiAlert from "@material-ui/lab/Alert"
+import { Snackbar } from "@material-ui/core"
+import Verify from "./verify"
+
+function Alert({ open, severity, message, onClose }) {
+    return (
+        <Snackbar
+            anchorOrigin={{ vertical: "bottom", horizontal: "center" }}
+            autoHideDuration={6000}
+            open={open}
+            onClose={onClose}
+        >
+            <MuiAlert elevation={6} variant="filled" severity={severity}>
+                {message}
+            </MuiAlert>
+        </Snackbar>
+    )
+}
+
+const isWrongPasswordOrEmail = code => ["auth/user-not-found", "auth/wrong-password"].includes(code)
+const isWeakPassword = code => ["auth/weak-password"].includes(code)
+const isAccountExists = code => ["auth/email-already-in-use"].includes(code)
+
+function readableErrorMessage(code, message) {
+    if (isWrongPasswordOrEmail(code)) {
+        return "Wrong Username or Password!"
+    }
+    if (isWeakPassword(code)) {
+        return "Password to weak!"
+    }
+    if (isAccountExists(code)) {
+        return "The email address is already in use."
+    }
+    if (code === "verify/spam-protection") {
+        return "Already send verification email in the last 5 min"
+    }
+    if (code === "verify/already-verified") {
+        return "Already verified"
+    }
+    return `${code} - ${message}`
+}
+
+const screens = {
+    LOGIN: "login",
+    REGISTER: "register",
+    VERIFY: "verify",
+}
 
 export const LoginRegisterForm = ({ loginSuccessUrl }) => {
-    const [showLogin, setShowLogin] = useState(true)
-    const switchScreen = () => setShowLogin(!showLogin)
-    const { initializing, user } = useAuth(firebase)
+    const [showScreen, setShowScreen] = useState(screens.LOGIN)
+    const switchToLogin = () => setShowScreen(screens.LOGIN)
+    const switchToRegister = () => setShowScreen(screens.REGISTER)
+    const { user } = useAuth(firebase)
+    const [alert, setAlert] = useState({
+        open: false,
+        message: "",
+        severity: "error",
+    })
 
     if (user) {
-        navigate(loginSuccessUrl)
+        if (user.emailVerified) {
+            navigate(loginSuccessUrl)
+        }
+        if (showScreen !== screens.VERIFY) {
+            setShowScreen(screens.VERIFY)
+        }
     }
 
-    if (initializing) {
-        return <h1>initializing</h1>
+    const onError = (code, message) => {
+        setAlert({
+            open: true,
+            message: readableErrorMessage(code, message),
+            severity: "error",
+        })
     }
+
+    const onSuccess = user => {
+        if (user?.emailVerified) {
+            navigate(loginSuccessUrl)
+        }
+        setShowScreen(screens.VERIFY)
+    }
+
+    const onVerifySuccess = message => {
+        setAlert({
+            open: true,
+            message,
+            severity: "info",
+        })
+    }
+
+    const closeAlert = () => setAlert({ ...alert, open: false })
 
     return (
         <>
-            {showLogin && [
-                <Login key="login" successUrl={loginSuccessUrl} />,
+            <Alert {...alert} onClose={closeAlert} />
+            {showScreen === screens.LOGIN && [
+                <Login key="login" onSuccess={onSuccess} onError={onError} />,
                 <div key="doNotHaveAccountKey">
-                    Don't have an Account? <a onClick={switchScreen}>Sign up</a>
+                    Don't have an Account? <a onClick={switchToRegister}>Sign up</a>
                 </div>,
             ]}
-            {!showLogin && [
-                <Register key="register" />,
+            {showScreen === screens.REGISTER && [
+                <Register key="register" onError={onError} onSuccess={onSuccess} />,
                 <div key="doHaveAccountKey">
-                    Already have an Account? <a onClick={switchScreen}>Sign in</a>
+                    Already have an Account? <a onClick={switchToLogin}>Sign in</a>
                 </div>,
             ]}
+            {showScreen === screens.VERIFY && <Verify key="verify" onError={onError} onSuccess={onVerifySuccess} />}
         </>
     )
 }
