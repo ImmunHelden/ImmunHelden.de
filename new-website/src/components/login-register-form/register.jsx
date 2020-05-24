1d53524c300fffdf5d43356661e9cc647207d1c8
David Kagerer <david@binder-kagerer.de>
2020-05-24 14:19:19 +0200

    Feature/add signup (#97)
    * feat(functions): :sparkles: add automatic creation and deletion of users in db
    
    * feat(partner): :sparkles: add partner to provider
    
    * feat(partner): :sparkles: fetch data from partners from firestore
    
    * refactor(functions): :fire: remove email from user collection
    
    * feat(partner): :sparkles: add signup for partners

--- new-website/src/components/login-register-form/register.jsx
+++ new-website/src/components/login-register-form/register.jsx
@@ -2,6 +2,13 @@ import React from "react"
 import { Paper, withStyles, Grid, TextField, Button, FormControlLabel, Checkbox } from "@material-ui/core"
 import { Face, Fingerprint } from "@material-ui/icons"
 import { makeStyles } from "@material-ui/styles"
+import MuiAlert from "@material-ui/lab/Alert"
+import { useForm } from "react-hook-form"
+import firebase from "gatsby-plugin-firebase"
+
+function Alert(props) {
+    return <MuiAlert elevation={6} variant="filled" {...props} />
+}
 
 const useStyles = makeStyles(theme => ({
     margin: {
@@ -14,15 +21,39 @@ const useStyles = makeStyles(theme => ({
 
 const Register = () => {
     const classes = useStyles()
+    const { register, handleSubmit } = useForm()
+
+    const onSubmit = ({ email, password }) => {
+        firebase
+            .auth()
+            .createUserWithEmailAndPassword(email, password)
+            .then(() => <Alert severity="success">Account Created! Please verify Email!</Alert>)
+            .catch(error => {
+                const errorCode = error.code
+                const errorMessage = error.message
+                console.log({ errorCode, errorMessage })
+                return <Alert severity="error">Something went wrong please try again later!</Alert>
+            })
+    }
+
     return (
         <Paper className={classes.padding}>
-            <div className={classes.margin}>
+            <form onSubmit={handleSubmit(onSubmit)} className={classes.margin}>
                 <Grid container spacing={8} alignItems="flex-end">
                     <Grid item>
                         <Face />
                     </Grid>
                     <Grid item md={true} sm={true} xs={true}>
-                        <TextField id="email" label="Email" type="email" fullWidth autoFocus required />
+                        <TextField
+                            id="email"
+                            label="Email"
+                            type="email"
+                            name="email"
+                            fullWidth
+                            autoFocus
+                            required
+                            inputRef={register({ required: true })}
+                        />
                     </Grid>
                 </Grid>
                 <Grid container spacing={8} alignItems="flex-end">
@@ -30,15 +61,23 @@ const Register = () => {
                         <Fingerprint />
                     </Grid>
                     <Grid item md={true} sm={true} xs={true}>
-                        <TextField id="password" label="Password" type="password" fullWidth required />
+                        <TextField
+                            id="password"
+                            label="Password"
+                            type="password"
+                            name="password"
+                            fullWidth
+                            required
+                            inputRef={register({ required: true })}
+                        />
                     </Grid>
                 </Grid>
                 <Grid container justify="center" style={{ marginTop: "10px" }}>
-                    <Button variant="outlined" color="primary" style={{ textTransform: "none" }}>
+                    <Button type="submit" variant="outlined" color="primary" style={{ textTransform: "none" }}>
                         Register
                     </Button>
                 </Grid>
-            </div>
+            </form>
         </Paper>
     )
 }
