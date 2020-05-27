ea54509610eee53321c41bceae32ce86d5b1be39
David Kagerer <david@binder-kagerer.de>
2020-05-27 23:45:23 +0200

    refactor(partner-area): :recycle: tidy code and introduce firebase hooks

---
+++ new-website/src/util/location.js
@@ -0,0 +1,18 @@
+export function mapLocation(prev, doc) {
+    const { title = "NA", latlng = {}, address = "NA", phone = "NA", email = "NA", contact = "NA", id = "NA" } = doc
+    const { latitude, longitude } = latlng
+    return [
+        ...prev,
+        {
+            title,
+            latitude,
+            longitude,
+            latlng,
+            address,
+            phone,
+            email,
+            contact,
+            id,
+        },
+    ]
+}
