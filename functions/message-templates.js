1e2df6be5397dcde9d9aa5f654a0c2442134d064
Stefan Gränitz <stefan.graenitz@gmail.com>
2020-06-09 10:16:57 +0200

    First draft FAQ and utils for rendering

--- functions/message-templates.js
+++ functions/message-templates.js
@@ -4,6 +4,16 @@ const crypto = require('crypto');
 const showdown = require('showdown');
 const fillTemplate = require('es6-dynamic-template');
 
+showdown.extension('header-anchors', function() {
+  const ancTpl = '$1<a id="user-content-$3" class="anchor" href="#$3" aria-hidden="true"><svg aria-hidden="true" class="octicon octicon-link" height="16" version="1.1" viewBox="0 0 16 16" width="16"><path fill-rule="evenodd" d="M4 9h1v1H4c-1.5 0-3-1.69-3-3.5S2.55 3 4 3h4c1.45 0 3 1.69 3 3.5 0 1.41-.91 2.72-2 3.25V8.59c.58-.45 1-1.27 1-2.09C10 5.22 8.98 4 8 4H4c-.98 0-2 1.22-2 2.5S3 9 4 9zm9-3h-1v1h1c1 0 2 1.22 2 2.5S13.98 12 13 12H9c-.98 0-2-1.22-2-2.5 0-.83.42-1.64 1-2.09V6.25c-1.09.53-2 1.84-2 3.25C6 11.31 7.55 13 9 13h4c1.45 0 3-1.69 3-3.5S14.5 6 13 6z"></path></svg></a>$4';
+
+  return [{
+    type: 'html',
+    regex: /(<h(3) id="([^"]+?)">)(.*<\/h\2>)/g,
+    replace: ancTpl
+  }];
+});
+
 exports.render = async function(template, params) {
   const baseUrl = 'https://raw.githubusercontent.com/ImmunHelden/ImmunHelden.de/notifications/';
   const template_markdown = await rp.get({ uri: baseUrl + template });
@@ -87,4 +97,14 @@ exports.doSendExampleMail = functions.https.onRequest(async (req, res) => {
   catch (err) {
     res.status(400).send(err.message);
   }
+});
+
+exports.renderFaq = functions.https.onRequest(async (req, res) => {
+  const url = 'https://raw.githubusercontent.com/ImmunHelden/ImmunHelden.de/markdown/faq/faq.md';
+  const markdown = await rp.get({ uri: url });
+  const conv = new showdown.Converter({
+    extensions: ['header-anchors']
+  });
+  conv.setFlavor('github');
+  res.send(conv.makeHtml(markdown));
 });
