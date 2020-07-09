add3ce2a500e21dd1a3023f51080612522d41c03
Stefan Gränitz <stefan.graenitz@gmail.com>
2020-07-09 15:47:52 +0200

    [Partners] Aggregate state on edit page into one single variable

--- new-website/src/components/location/edit-form.jsx
+++ new-website/src/components/location/edit-form.jsx
@@ -16,30 +16,23 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 export const EditForm = ({ docId, doc, onError }) => {
-  const classes = useStyles()
-  const { formatMessage } = useIntl()
-
-  const [title, setTitle] = useState(doc.title || "")
-  const [address, setAddress] = useState(doc.address || "")
-  const [phone, setPhone] = useState(doc.phone || "")
-  const [email, setEmail] = useState(doc.email || "")
-  const [contact, setContact] = useState(doc.contact || "")
+  const [state, setState] = React.useState({
+    title: doc.title || "",
+    address: doc.address || "",
+    phone: doc.phone || "",
+    email: doc.email || "",
+    contact: doc.contact || "",
+  })
 
   async function handleSubmit(event) {
     event.preventDefault()
     try {
       // Throws explicitly if the title is empty.
-      if (!title || title.length === 0)
+      if (!state.title || state.title.length === 0)
         throw new Error("requiredFieldMissing/title")
 
       // Blocks until the update operation is complete and throws if it failed.
-      await firebase.firestore().collection(LOCATION_COLLECTION).doc(docId).update({
-        title: title,
-        address: address,
-        phone: phone,
-        email: email,
-        contact: contact,
-      })
+      await firebase.firestore().collection(LOCATION_COLLECTION).doc(docId).update(state)
 
       navigate("/partner/", {
         replace: true,
@@ -51,15 +44,22 @@ export const EditForm = ({ docId, doc, onError }) => {
     }
   }
 
+  const handleChange = (event) => {
+    setState({ ...state, [event.target.name]: event.target.value })
+  }
+
+  const classes = useStyles()
+  const { formatMessage } = useIntl()
+
   return (
     <form className={classes.root} onSubmit={handleSubmit}>
       <TextField disabled fullWidth label={formatMessage({ id: "partnerLocation_Anchor" })} value={"#" + docId} />
       <TextField disabled fullWidth label={formatMessage({ id: "partnerLocation_PartnerID" })} value={doc.partnerId} />
-      <TextField fullWidth label={formatMessage({ id: "partnerLocation_Title" })} value={title} onChange={e => setTitle(e.target.value)} />
-      <TextField fullWidth label={formatMessage({ id: "partnerLocation_Address" })} value={address} onChange={e => setAddress(e.target.value)} />
-      <TextField fullWidth label={formatMessage({ id: "partnerLocation_Phone" })} value={phone} onChange={e => setPhone(e.target.value)} />
-      <TextField fullWidth label={formatMessage({ id: "partnerLocation_Email" })} value={email} onChange={e => setEmail(e.target.value)} />
-      <TextField fullWidth label={formatMessage({ id: "partnerLocation_Contact" })} value={contact} onChange={e => setContact(e.target.value)} />
+      <TextField fullWidth name="title" label={formatMessage({ id: "partnerLocation_Title" })} defaultValue={state.title} onChange={handleChange} />
+      <TextField fullWidth name="address" label={formatMessage({ id: "partnerLocation_Address" })} defaultValue={state.address} onChange={handleChange} />
+      <TextField fullWidth name="phone" label={formatMessage({ id: "partnerLocation_Phone" })} defaultValue={state.phone} onChange={handleChange} />
+      <TextField fullWidth name="email" label={formatMessage({ id: "partnerLocation_Email" })} defaultValue={state.email} onChange={handleChange} />
+      <TextField fullWidth name="contact" label={formatMessage({ id: "partnerLocation_Contact" })} defaultValue={state.contact} onChange={handleChange} />
       <Button type="submit" variant="contained" color="primary" size="large" startIcon={<SaveIcon />}>
         Save
       </Button>
