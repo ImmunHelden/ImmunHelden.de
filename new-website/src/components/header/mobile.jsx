365a9994bb47a72dd932cd40e58710fe64e20ccd
David Kagerer <david@binder-kagerer.de>
2020-07-13 22:55:46 +0200

    ⚠️first draft mobile header

---
+++ new-website/src/components/header/mobile.jsx
@@ -0,0 +1,20 @@
+import React from "React";
+import { makeStyles } from "@material-ui/styles";
+import LogoSVG from "../../images/svg/logo.svg";
+import { Toolbar } from "@material-ui/core";
+
+const useStyles = makeStyles(() => ({
+    container: {
+        backgroundColor: "white",
+    },
+}));
+
+export const MobileHeader = () => {
+    const classes = useStyles();
+    return (
+        <Toolbar className={classes.container}>
+            <LogoSVG height="50" width="90" />
+        </Toolbar>
+    );
+};
+
