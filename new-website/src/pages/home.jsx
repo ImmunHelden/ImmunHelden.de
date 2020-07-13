ceaec4de29447902257e9c953e0ba0aca0f161ba
David Kagerer <david@binder-kagerer.de>
2020-07-13 22:55:16 +0200

    ✨add basic video and map lane

--- new-website/src/pages/home.jsx
+++ new-website/src/pages/home.jsx
@@ -5,6 +5,7 @@ import HeroButton from "../components/hero/HeroButton";
 import SupportedByLane from "../components/supported/SupportedByLane";
 import { Typography, Grid } from "@material-ui/core";
 import { graphql } from "gatsby";
+import Img from "gatsby-image";
 
 const IndexPage = ({ data }) => {
     return (
@@ -24,6 +25,60 @@ const IndexPage = ({ data }) => {
                     </div>
                 </Hero>
                 <SupportedByLane />
+                <div
+                    style={{
+                        backgroundColor: "#FC4141",
+                        height: "70vh",
+                        margin: "10px 55px",
+                        borderRadius: "20px",
+                        color: "white",
+                        textAlign: "center",
+                    }}
+                >
+                    <Img
+                        fluid={data.videoTeaserImage.edges[0].node.fluid}
+                        style={{ width: "calc(100% / 2)", left: 0, right: 0, margin: "auto" }}
+                    />
+                    <Typography variant="h2">mit heldentaten durch die corona pandemie</Typography>
+                    <Typography variant="h6">
+                        Wir erklären dir kurz und verständlich, warum genesene Patient:innen eine echte Chance für
+                        unsere Gesellschaft in der aktuelle Situation darstellen.
+                    </Typography>
+                    <HeroButton color="default">⌲ Erklärvideo ansehen</HeroButton>
+                </div>
+                <div
+                    style={{
+                        backgroundColor: "lightblue",
+                        height: "70vh",
+                        textAlign: "center",
+                    }}
+                >
+                    <div
+                        style={{
+                            width: "calc(100% / 2)",
+                            backgroundColor: "white",
+                            borderRadius: "13px",
+                            textAlign: "left",
+                            position: "relative",
+                            top: "calc(50% - 8rem)",
+                            height: "16rem",
+                            margin: "0 55px",
+
+                            padding: "1rem",
+                        }}
+                    >
+                        <Typography variant="h2">Blutplasmaspende</Typography>
+                        <Typography variant="h6">
+                            Während deines Kampfes gegen COVID-19 hat dein Immunsystem Antikörper gegen das Virus
+                            gebildet. Sie haben dir geholfen, das Virus zu besiegen und finden sich nun in deinem
+                            Blutplasma. Wenn du alle gesundheitlichen Kriterien erfüllst, kannst du mit deiner Spende
+                            schwer Erkrankten im Kampf gegen COVID-19 helfen. Oder einen Beitrag zur Forschung über
+                            Antikörper leisten. Eine passende Spendeeinrichtung in Deiner Nähe findest du auf unserer
+                            ImmunHelden Map.
+                        </Typography>
+                    </div>
+                    <HeroButton>Standort in meiner Nähe finden</HeroButton>
+                </div>
             </div>
         </LayoutFancy>
     );
@@ -33,6 +88,16 @@ export default IndexPage;
 
 export const pageQuery = graphql`
     query {
+        videoTeaserImage: allImageSharp(filter: { original: { src: { regex: "/video/" } } }) {
+            edges {
+                node {
+                    id
+                    fluid(quality: 100) {
+                        ...GatsbyImageSharpFluid_withWebp
+                    }
+                }
+            }
+        }
         hero: allImageSharp(filter: { original: { src: { regex: "/banner-image/" } } }) {
             edges {
                 node {
