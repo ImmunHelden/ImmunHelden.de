346000143318ebe79a6bb211f8bbb1330dd594ae
Stefan Gränitz <stefan.graenitz@gmail.com>
2020-07-15 18:08:41 +0200

    [Partners] Resolve address to GeoPoint and allow setting the latter explicitly

--- new-website/src/components/location/edit-form.jsx
+++ new-website/src/components/location/edit-form.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react"
 import { makeStyles } from '@material-ui/core/styles';
-import { TextField, Button, Checkbox, FormControlLabel, FormGroup } from '@material-ui/core';
+import { TextField, Button, Checkbox, Switch, FormControlLabel, FormGroup } from '@material-ui/core';
 import firebase from "gatsby-plugin-firebase"
 import { navigate, useIntl } from "gatsby-plugin-intl"
 import SaveIcon from '@material-ui/icons/Save';
@@ -30,6 +30,46 @@ const useStyles = makeStyles((theme) => ({
   },
 }))
 
+// Resolve coordinates for given address with LocationIQ
+async function requestLatLng(address) {
+  const sleep = async (ms) => {
+    await new Promise((wakeup, _) => setTimeout(wakeup, ms));
+  };
+  const tryResolve = async (address, retries) => {
+    const STATUS_CODE_RATE_LIMIT_EXCEEDED = 429
+    for (let i = 0; i < retries; i++) {
+      await sleep(500); // rate limit second
+      try {
+        // TODO: Easy way to hide our key? (not urgent, it's a free account)
+        const baseUrl = "https://eu1.locationiq.com/v1/search.php"
+        const queryBase = "key=pk.861b8037ac48a2b23d05c90e89658064&format=json&q="
+        const response = await fetch(baseUrl + "?" + queryBase + address)
+        return await response.json()
+      } catch (err) {
+        if (err.statusCode === STATUS_CODE_RATE_LIMIT_EXCEEDED) {
+          // Report warning and try again
+          console.warn("Rate limit reached resolving address ", address, ":", err)
+        } else {
+          // Unknown error
+          throw err
+        }
+      }
+    }
+    throw new Error("resolutionFailure/timeout")
+  }
+
+  const matches = await tryResolve(address, 3)
+  const haveAtLeastOneResult =
+      matches.hasOwnProperty("length") && matches.length > 0 &&
+      matches[0].hasOwnProperty("lat") && matches[0].hasOwnProperty("lon")
+
+  if (!haveAtLeastOneResult)
+    throw new Error("resolutionFailure/notFound")
+
+  // Success
+  return [parseFloat(matches[0].lat), parseFloat(matches[0].lon)]
+}
+
 export const EditForm = ({ docId, doc, onError }) => {
   const classes = useStyles()
   const { formatMessage } = useIntl()
@@ -44,16 +84,41 @@ export const EditForm = ({ docId, doc, onError }) => {
     phone: doc.phone || "",
     email: doc.email || "",
     published: doc.published || false,
+    latlngExplicit: doc.latlngExplicit || false,
+    latlng: doc.latlng || { latitude: 0, longitude: 0 },
   })
+
+  const [explLat, setExplLat] = useState(state.latlng.latitude)
+  const [explLng, setExplLng] = useState(state.latlng.longitude)
 
   async function handleSubmit(event) {
     event.preventDefault()
     try {
-      // Throws explicitly if the title is empty.
-      if (!state.title || state.title.length === 0)
-        throw new Error("requiredFieldMissing/title")
+      let coord = [0, 0]
+      if (state.latlngExplicit) {
+        // Make sure we have numbers in both fields.
+        if (!explLat || isNaN(explLat))
+          throw new Error("requiredFieldMissing/lat")
+        if (!explLng || isNaN(explLng))
+          throw new Error("requiredFieldMissing/lng")
+        coord = [parseFloat(explLat), parseFloat(explLng)]
+      } else if (state.address.length > 0) {
+        // Resolve location for given address (or throw if we don't get one).
+        coord = await requestLatLng(state.address)
+        console.log("Resolved coordinates for address", state.address,
+                    "as:", coord)
+      }
 
+      state.latlng = new firebase.firestore.GeoPoint(coord[0], coord[1])
       state.description = makeSnapshot(editorState)
+
+      // Throws explicitly if the title is empty.
+      if (state.published) {
+        if (state.title.length === 0)
+          throw new Error("requiredFieldMissing/title")
+        if (coord === [0, 0])
+          throw new Error("requiredFieldMissing/address")
+      }
 
       // Blocks until the update operation is complete and throws if it failed.
       await firebase.firestore().collection(LOCATION_COLLECTION).doc(docId).update(state)
@@ -74,6 +139,31 @@ export const EditForm = ({ docId, doc, onError }) => {
   const handleChecked = (event) => {
     setState({ ...state, [event.target.name]: event.target.checked })
   }
+  const handleExplLatLng = async (event) => {
+    handleChecked(event)
+
+    // We are done here, if we (1) switched of, (2) have stored values for
+    // latlng, (3) have unsaved values for latlng, or (4) have no address.
+    if (!event.target.checked)
+      return
+    if (state.latlng.latitude !== 0 && state.latlng.longitude !== 0)
+      return
+    if (explLat !== 0 && explLng !== 0)
+      return
+    if (state.address.length === 0)
+      return
+
+    // Otherwise try to resolve and fill in location of current address.
+    try {
+      const [lat, lng] = await requestLatLng(state.address)
+      console.log("Resolved coordinates for address", state.address,
+                  "as:", [lat, lng])
+      setExplLat(lat)
+      setExplLng(lng)
+    } catch (err) {
+      onError(err)
+    }
+  }
 
   return (
     <form className={classes.root} onSubmit={handleSubmit}>
@@ -82,12 +172,41 @@ export const EditForm = ({ docId, doc, onError }) => {
         <TextField disabled label={formatMessage({ id: "partnerLocation_PartnerID" })} value={doc.partnerId} />
       </FormGroup>
       <FormGroup className={classes.row}>
-        <TextField name="title" label={formatMessage({ id: "partnerLocation_Title" })} defaultValue={state.title} onChange={handleChange} />
-        <TextField name="address" label={formatMessage({ id: "partnerLocation_Address" })} defaultValue={state.address} onChange={handleChange} />
+        <TextField name="title" label={formatMessage({ id: "partnerLocation_Title" })} value={state.title} onChange={handleChange} />
       </FormGroup>
       <FormGroup className={classes.row}>
-        <TextField name="phone" label={formatMessage({ id: "partnerLocation_Phone" })} defaultValue={state.phone} onChange={handleChange} />
-        <TextField name="email" label={formatMessage({ id: "partnerLocation_Email" })} defaultValue={state.email} onChange={handleChange} />
+        <TextField name="phone" label={formatMessage({ id: "partnerLocation_Phone" })} value={state.phone} onChange={handleChange} />
+        <TextField name="email" label={formatMessage({ id: "partnerLocation_Email" })} value={state.email} onChange={handleChange} />
+      </FormGroup>
+      <FormGroup className={classes.row}>
+        <TextField name="address" label={formatMessage({ id: "partnerLocation_Address" })} value={state.address} onChange={handleChange} />
+      </FormGroup>
+      <FormGroup className={classes.row}>
+        <FormControlLabel
+          control={
+            <Switch
+              name="latlngExplicit"
+              color="primary"
+              checked={state.latlngExplicit}
+              onChange={handleExplLatLng}
+            />
+          }
+          label={formatMessage({ id: "partnerLocation_ExplicitPinPosition" })}
+        />
+        {state.latlngExplicit && (
+          <>
+            <TextField
+              value={explLat}
+              onChange={e => setExplLat(e.target.value)}
+              label={formatMessage({ id: "partnerLocation_Latitude" })}
+            />
+            <TextField
+              value={explLng}
+              onChange={e => setExplLng(e.target.value)}
+              label={formatMessage({ id: "partnerLocation_Longitude" })}
+            />
+          </>
+        )}
       </FormGroup>
       <FormControlLabel
         className={classes.editorLabel}
