98aa901f8ea6b8bd22e76efb36cd1cd1aded4aa9
Stefan Gränitz <stefan.graenitz@gmail.com>
2020-07-17 18:58:52 +0200

    [Map] Update endpoints for refined location data

--- functions/map.js
+++ functions/map.js
@@ -1,55 +1,75 @@
 const cors = require("cors")({
   origin: true,
-})
+});
+
+function error(res, logText) {
+  // TODO: How can we log it in Sentry?
+  res.status(400).send("Invalid request")
+  if (logText)
+    console.error(logText)
+}
 
 exports.pin_locations = function(admin, req, res) {
   // Apparently we have to extract the 'type' parameter from the URL manually.
   // E.g. req.params is { '0': '/maps/sample/pins' } => type is 'sample'
-  const match = /\/maps\/(.*)\/pins/.exec(req.params['0'])
-  const type = match[1]
+  const match = /\/maps\/(.*)\/pins/.exec(req.params['0']);
+  const type = match[1];
 
-  // This endpoint supports cross-origin requests.
+  // Support cross-origin requests.
   return cors(req, res, async () => {
-    const locationsRef = admin.firestore().collection("locations")
-    const entries = await locationsRef.where('type', '==', type).get()
-    const pins = {}
+    const locationsRef = admin.firestore().collection("locations");
+    const entries = await locationsRef.where('type', '==', type).get();
+    const pins = {};
     entries.forEach(doc => {
-      const geoPoint = doc.get('latlng')
-      pins[doc.id] = {
-        title: doc.get('title'),
-        latlng: [geoPoint.latitude, geoPoint.longitude],
+      const fields = doc.data();
+      if (fields.published) {
+        pins[doc.id] = {
+          title: fields.title,
+          latlng: [fields.latlng.latitude, fields.latlng.longitude],
+        };
       }
-    })
+    });
 
-    console.log('Serving', Object.keys(pins).length, 'locations in response to query', req.params[0])
-    res.json(pins).send()
+    res.json(pins).send();
+    console.log('Served', Object.keys(pins).length,
+                'locations in response to query', req.params[0]);
   })
 }
 
 exports.details_html = function(admin, req, res) {
-  // This endpoint supports cross-origin requests.
+  // Apparently we have to extract the 'type' and 'id' parameters from the URL
+  // manually, with req.params['0'] looking like: `/maps/type/details_html/id`
+  const uri = req.params['0'];
+  const match = /\/maps\/(.*)\/details\/(.*)/.exec(uri);
+  if (!match || match.length !== 3)
+    return invalid(res);
+
+  const type = match[1];
+  const id = match[2];
+
+  // Support cross-origin requests.
   return cors(req, res, async () => {
-    // TODO: Figure out how to access the ID and grab the respective document.
-    const doc = await admin.firestore().collection("plasma").doc("VUoS00fHyd3q5Qow5m8h").get()
-    const fields = doc.data()
+    const doc = await admin.firestore().collection("locations").doc(id).get();
+    if (!doc.exists)
+      return invalid(res, `Requested doc does't exist: ${uri}`);
+
+    const fields = doc.data();
+    if (!fields.published)
+      return invalid(res, `Requested doc not published: ${uri}`);
+
+    if (!fields.description || !fields.addendum)
+      console.error("Requested doc doesn't have all required fields:", uri);
+
     res.send(`
       <html>
       <body>
         <div>
-          ${fields.description}
+          ${fields.description || ""}
           <br>
-          ${fields.addendum}
+          ${fields.addendum || ""}
         </div>
       </body>
       </html>
     `)
-  })
-}
-
-// Currently unused.
-exports.regions = function(req, res) {
-  // This endpoint supports cross-origin requests.
-  return cors(req, res, () => {
-    res.json({})
   })
 }
