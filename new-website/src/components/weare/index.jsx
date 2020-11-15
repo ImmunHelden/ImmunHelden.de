537d40931c240eed1cc1941a613356cc83202a61
edfii <edith.fink@gmx.net>
2020-11-15 10:57:33 +0100

    optimization of 'we are' snippet

--- new-website/src/components/weare/index.jsx
+++ new-website/src/components/weare/index.jsx
@@ -6,26 +6,42 @@ import schloss from "../../images/icons/schloss.png"
 import team from "../../images/icons/team.png"
 
 const useStyles = makeStyles(theme => ({
-    color: {
-        color: "#FC4141",
-        paddingBottom: 40
-    },
     border: {
       border: 3 ,
       borderColor: "#FC4141",
       borderRadius: 15,
       borderStyle: "solid",
       marginTop: 40,
-      marginBottom: 40
+      marginBottom: 40,
+      marginLeft:24,
+      marginRight:24,
+      width:"auto"
+    },
+    color:{
+      color: "#FC4141",
+      paddingBottom: 40,
+      
     },
     inline: {
       display: "flex",
       alignItems: "center",
-      justifyContent: "center"
+      justifyContent: "center",
+      [theme.breakpoints.only('sm')]: {
+        display: "grid",
+      },
     },
     spacing: {
-      paddingLeft:10
-    }
+      paddingLeft:10,
+      [theme.breakpoints.only('sm')]: {
+        padding: "0px",
+        margin: "0px"
+      },      
+    },
+    img: {
+      [theme.breakpoints.only('sm')]: {
+        margin: "auto",
+        },
+      },
 
 }))
 
@@ -37,15 +53,15 @@ export default function Weare() {
     <h3><FormattedMessage id="weare"/></h3>
     <Grid container spacing={3}>
     <Grid className={classes.inline} xs={12} sm={4}>
-      <img src={team} alt="Ehrenamtlich" />
+      <img className={classes.img} src={team} alt="Ehrenamtlich" />
       <p className={classes.spacing}><FormattedMessage id="honorary"/></p>
     </Grid>
     <Grid className={classes.inline} xs={12} sm={4}>
-     <img src={hand} alt="Uneigennützig" />
+     <img className={classes.img} src={hand} alt="Uneigennützig" />
      <p className={classes.spacing}><FormattedMessage id="unselfish"/></p>
     </Grid>
     <Grid className={classes.inline} xs={12} sm={4}>
-      <img src={schloss} alt="Open Source" />
+      <img className={classes.img} src={schloss} alt="Open Source" />
       <p className={classes.spacing}><FormattedMessage id="opensource"/></p>
     </Grid>
     </Grid>
