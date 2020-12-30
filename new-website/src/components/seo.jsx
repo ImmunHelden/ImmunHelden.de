40501635c9fb0d6bb7f7d3e4c64cce1f1783317c
edfii <edith.fink@gmx.net>
2020-12-30 20:30:25 +0100

    add viewport

--- new-website/src/components/seo.jsx
+++ new-website/src/components/seo.jsx
@@ -62,6 +62,10 @@ function SEO({ description, lang, meta, title }) {
                     name: `twitter:description`,
                     content: metaDescription,
                 },
+                {
+                    name: `viewport`,
+                    content: 'width=device-width, initial-scale=1.0',
+                },
             ].concat(meta)}
         />
     )
