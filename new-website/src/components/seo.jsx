3f3cfd293f1d2cbab3c0d2913dc539f28c46ae1c
edfii <edith.fink@gmx.net>
2020-12-31 10:07:04 +0100

    overflow-x for mobile

--- new-website/src/components/seo.jsx
+++ new-website/src/components/seo.jsx
@@ -62,10 +62,6 @@ function SEO({ description, lang, meta, title }) {
                     name: `twitter:description`,
                     content: metaDescription,
                 },
-                {
-                    name: `viewport`,
-                    content: 'width=device-width, initial-scale=1.0',
-                },
             ].concat(meta)}
         />
     )
